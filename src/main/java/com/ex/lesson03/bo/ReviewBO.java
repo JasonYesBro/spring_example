819895b77961f22cd3051275c5dd668a817be74e
JasonYesBro <spdlqjqlwjs@naver.com>
2023-03-22 21:42:18 +0900

    fix : insert 연습

--- src/main/java/com/ex/lesson03/bo/ReviewBO.java
+++ src/main/java/com/ex/lesson03/bo/ReviewBO.java
@@ -1,11 +1,8 @@
 package com.ex.lesson03.bo;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ex.lesson02.model.UsedGoods;
 import com.ex.lesson03.dao.ReviewMapper;
 import com.ex.lesson03.model.Review;
 
@@ -20,5 +17,19 @@ public class ReviewBO {
 	public Review getReview(int id) {
 		
 		return reviewMapper.selectReview(id);
+	}
+	
+	// input : Review   output : 성공한 행의 개수 int
+	// BO 서비스 레이어이기 때문에 메서드명 add 임
+	public int addReview(Review review) {
+		
+		return reviewMapper.insertReview(review);
+	}
+
+	// 관례적으로 컬럼명과 변수명 동일시 함
+	// point는 null 이 허용이기 때문에 Wrapper 클래스로 정의
+	public int addReviewAsField(int storeId111, String menu111, String userName111, Double point111, String review111) {
+		
+		return reviewMapper.insertReviewAsField(storeId111, menu111, userName111, point111, review111);
 	}
 }
--- src/main/java/com/ex/lesson03/dao/ReviewMapper.java
+++ src/main/java/com/ex/lesson03/dao/ReviewMapper.java
@@ -8,4 +8,17 @@ import com.ex.lesson03.model.Review;
 @Repository
 public interface ReviewMapper {
 	public Review selectReview(@Param("id") int id);
+	
+	// DB 와 가까운 레이어이기 때문에 메서드명 insert -> insert가 수행되고 나면 알아서 mybatis 가 수행결과 개수를 알려줌
+	public int insertReview(Review review);
+	
+	// @Param 어노테이션이 있기 때문에 하나의 Map 객체가 넘어가는 것과 같음.
+	// 여러개의 파라미터가 넘어가기 때문에 @Param 어노테이션 붙인다.
+	// @Param으로 넘어가는 value와 mapper.xml 에 들어가는 데이터 값과 무조건 같아야함!
+	public int insertReviewAsField(
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point,
+			@Param("review") String review); 
 }
